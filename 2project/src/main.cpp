6819978758f8e558b86313b5b11fa1e6a798c38c
indexhtml821 <isaacopine@gmail.com>
2022-05-24 11:14:21 -0600

    se cambia el main para que se acorde arequerimento

--- 2project/src/main.cpp
+++ 2project/src/main.cpp
@@ -1,8 +1,43 @@
+#include "procesador.h"
+#include <fstream>
 
 using namespace std;
 
-int main() {
+int main()
+{
+
+    string archivoPersonas = "personas.txt";
+    string archivoNomina = "nomina.txt";
+    string archivoHorasTrabajadas = "horastrabajadas.txt";
+
+    ifstream lectorPersonas(archivoPersonas, std::ifstream::in); // Por default abriendo como texto
+
+    if (!lectorPersonas.is_open())
+    {
+        std::cerr << "Error leyendo archivo ejemplo.txt" << std::endl;
+        return -1;
+    }
+
+    ifstream lectorNomina(archivoNomina, std::ifstream::in); // Por default abriendo como texto
+
+    if (!lectorNomina.is_open())
+    {
+        std::cerr << "Error leyendo archivo ejemplo.txt" << std::endl;
+        return -1;
+    }
+
+    ifstream lectorHorasTrabajadas(archivoHorasTrabajadas, std::ifstream::in); // Por default abriendo como texto
+
+    if (!lectorHorasTrabajadas.is_open())
+    {
+        std::cerr << "Error leyendo archivo ejemplo.txt" << std::endl;
+        return -1;
+    }
+
 
     
 
+    lectorPersonas.close();
+    lectorNomina.close();
+    lectorHorasTrabajadas.close();
 }
