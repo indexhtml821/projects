fa461116c8b42609d5d30e56758b49dfb268e6c5
indexhtml821 <isaacopine@gmail.com>
2022-05-25 23:54:08 -0600

    se prueba la función añadir empleados

--- 2project/tests/tests_procesador.cpp
+++ 2project/tests/tests_procesador.cpp
@@ -51,6 +51,55 @@ namespace
     string resultado = procesador.obtenerHoras(452);
 
     EXPECT_EQ(esperada, resultado);
-  } 
+  }
+
+  TEST(Tests_Procesador, Test_Anadir_Empleados)
+  {
+    /// AAA
+
+    // Arrange - configurar el escenario
+
+    ostringstream streamPruebaPersonas{};
+    streamPruebaPersonas << "1 Denis Tyler denis_tyler@biz.com 1 1" << endl;
+    streamPruebaPersonas << "2 Clarissa Parker clarissa_parker@biz.com 1 1" << endl;
+    streamPruebaPersonas << "487 Bart Noon Bart_Noon486@iatim.tech 2 236" << endl;
+    streamPruebaPersonas << "516 Rosa Tait Rosa_Tait1615@tonsy.org 2 254" << endl;
+
+    ostringstream streamPruebaNomina{};
+    streamPruebaNomina << "1 4600.9" << endl;
+    streamPruebaNomina << "2 3097.50" << endl;
+
+    ostringstream streamPruebaHoras{};
+    streamPruebaHoras << "487 45.91 60" << endl;
+    streamPruebaHoras << "516 47.20 20" << endl;
+
+    string pruebaPersonas = streamPruebaPersonas.str();
+    string pruebaNomina = streamPruebaNomina.str();
+    string pruebaHoras = streamPruebaHoras.str();
+
+    istringstream streamNomina(pruebaNomina);
+    istringstream streamPersonas(pruebaPersonas);
+    istringstream streamHoras(pruebaHoras);
+
+    Procesador procesador(&streamPersonas, &streamNomina, &streamHoras);
+
+    // Act - ejecute la operación`-
+    procesador.anadirSalariosHoras();
+    procesador.anadirSalariosNomina();
+    procesador.anadirEmpleados();
+
+    // Assert - valide los resultados
+
+    string esperada[4] = {"1 Denis Tyler denis_tyler@biz.com 1 1 4600.90 4278.837", "2 Clarissa Parker clarissa_parker@biz.com 1 1 3097.50 2880.675", "487 Bart Noon Bart_Noon486@iatim.tech 2 236 60 45.91 2754.6", "516 Rosa Tait Rosa_Tait1615@tonsy.org 2 254 20 47.20 944.0"};
+    int idResultado[4] = {1, 2, 487, 516};
+
+    
+        for (int i = 0; i < 4; i++)
+        {
+          string resultado = procesador.obtenerEmpleado(idResultado[i]);
+          EXPECT_EQ(esperada[i], resultado);
+        }
+
+    }
 
 }
