e72eda78765e7c14aaca001dd41b5129db79a9db
indexhtml821 <isaacopine@gmail.com>
2022-07-06 16:59:39 -0600

    91 memoria en wsl

---
+++ Practica/Libreria/src/calculadora.cpp
@@ -0,0 +1,8 @@
+#include "calculadora.h"
+
+using namespace Progra2;
+
+int Calculadora::Sumar(int numero1, int numero2)
+{
+    return numero1 + numero2;
+}
---
+++ Practica/Libreria/src/calculadora.h
@@ -0,0 +1,15 @@
+#ifndef CALCULADORA_H
+#define CALCULADORA_H
+
+namespace Progra2
+{
+    class Calculadora {
+
+        public:
+
+        int Sumar(int numero1, int numero2);
+
+    };
+}
+
+#endif
