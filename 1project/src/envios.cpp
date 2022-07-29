98714e6a4aab4ffcb6699752bcb7288d75747e0a
indexhtml821 <66281437+indexhtml821@users.noreply.github.com>
2022-07-28 19:19:56 -0600

    se quitan implementaciones de metodos

--- 1project/src/envios.cpp
+++ 1project/src/envios.cpp
@@ -1,35 +1,15 @@
-#include"envios.h"
-#include<iostream>
-#include<stdlib.h>
+#include "envios.h"
+#include <iostream>
+#include <stdlib.h>
 using namespace std;
-
 
 Envios::Envios(float kg, float km)
 {
 
     this->kg = kg;
     this->km = km;
-    
 }
 
 Envios::Envios()
 {
-
-    
-    
-}
-
-
-
-float Envios::CalcularCosto(float kg, float km){
-
-return kg*km;
-
-}
-
-
-float Envios::CalcularCosto(){
-
-return kg*km;
-
 }
