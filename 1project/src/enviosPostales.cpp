fb0d5ede789ae99d38a4a5c54c88cfed05a8c906
indexhtml821 <66281437+indexhtml821@users.noreply.github.com>
2022-07-28 19:23:27 -0600

    se indenta y se cubren errores en obtenerPrecio

--- 1project/src/enviosPostales.cpp
+++ 1project/src/enviosPostales.cpp
@@ -2,36 +2,38 @@
 
 using namespace std;
 
-EnviosPostales::EnviosPostales(float kg,float km, int clase):Envios(kg,km)
+EnviosPostales::EnviosPostales(float kg, float km, int clase) : Envios(kg, km)
 {
 
-this->kg=kg;
-this->km=km;
-this->clase=clase;
-
-    
+    this->kg = kg;
+    this->km = km;
+    this->clase = clase;
 }
 
-float EnviosPostales::CalcularCosto(){
+float EnviosPostales::CalcularCosto()
+{
 
-return kg * ( km * ObtenerPrecio( ));
+    return km * ObtenerPrecio();
 }
 
+float EnviosPostales::ObtenerPrecio()
+{
+    float precio = 0;
 
-
-float EnviosPostales::ObtenerPrecio(){
-    float precio= 0;
-
-    if ( 0 <= kg && kg <= 3)
+    if (0 <= kg && kg <= 3)
     {
         columna = 0;
-    }else if (4 <= kg && kg <= 8)
-    {
-         columna = 1;
-    }else if ( 9 <= kg)
-    {
-        columna = 3;
     }
-    precio=precioClase[clase][columna];
-    return  precio ;
+    else if (4 <= kg && kg <= 8)
+    {
+        columna = 1;
+    }
+    else if (9 <= kg)
+    {
+        columna = 2;
+    }
+
+    --clase;
+    precio = precioClase[clase][columna];
+    return precio;
 }
